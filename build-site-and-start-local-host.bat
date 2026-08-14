bbf5ce77d030403fd62a8ef1ac51785b328edb13
nikitapki <nisakov035@gmail.com>
2026-08-14 15:26:32 +0700

    Немного упрощения, чтобы постоянно не вспоминать команды для запуска, перезапуска сайта. Теперь есть батник, который упрощает задачу, достаточно его просто открыть. Способы открыть: - Скопировать путь к build-site-and-start-local-host.bat и ввести в Поиск на Windows или другими способами попасть к файлу через дирректории. И просто открыть его. - Упрощенный способ для пользователей vsCode. Использовать сочетание клавишь Ctrl + Shift + B. Запускает батник прямо в консоли vsCode'a

---
+++ build-site-and-start-local-host.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist "node_modules" (
+    call npm install
+)
+
+echo Clear dirrectory _site...
+if exist "_site" (
+    rmdir /s /q "_site"
+)
+
+echo Server starting...
+start /b "" node node_modules\@11ty\eleventy\cmd.cjs --serve <nul
+
+timeout /t 3 /nobreak >nul
+start http://localhost:8080
+
+echo Server started. From stopped close this window or use combination Ctrl + C.
+pause >nul
